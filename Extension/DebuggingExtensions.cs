7e418cedfa7c8cec07bcb9a3fc3b3ab7db933e49
BrynOvalles <brynovallesburgos@gmail.com>
2024-02-05 13:43:09 -0400

    Creando el Debugging

---
+++ Extension/DebuggingExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+namespace PizzaPlace.Extension;
+
+public static class DebuggingExtensions
+{
+	private static JsonSerializerOptions opciones = new JsonSerializerOptions { WriteIndented = true };
+	private static string ToJson(this object obj) => JsonSerializer.Serialize(obj, opciones);
+}
